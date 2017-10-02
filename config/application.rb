ba3774c08532b70601dab41acd9c33cdf69d5ff9
Tatsuya Ichikawa <tatsuyaichikawa@TatsuyanoMacBook-Pro.local>
2017-10-02 15:14:16 +0900

    edit_config/apprication.rb
    rails gコマンド使用時に、テスト、ヘルパー、JavaScriptファイル（coffeeファイル）などが生成されないようにコードを記述。

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module ChatSpace
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|  #不要なファイルの生成を抑止
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
