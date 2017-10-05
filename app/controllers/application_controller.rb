b0c3479590456f22990a426aa71bdfbabdd32e9b
Tatsuya Ichikawa <tatsuyaichikawa@TatsuyanoMacBook-Pro.local>
2017-10-05 14:35:11 +0900

    resolve_conflict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-
   def after_sign_out_path_for(resource)
     new_user_session_path
   end
