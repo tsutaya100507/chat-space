4184b2616189174158072c7909802794f5ef69d8
Tatsuya Ichikawa <tatsuyaichikawa@TatsuyanoMacBook-Pro.local>
2017-10-07 21:48:47 +0900

    edit_group_model for validatyion_setting
    グループ作成時グループ名の入力を必須にするためにバリデーションを設定した。

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,12 @@
 class Group < ApplicationRecord
+  # validate :add_error_sample
+
+  # def add_error_sample
+  #   if name.blank?
+  #     errors[:base] << "商品名は必ず入力して下さい"
+  #   end
+  # end
+  validates :name, presence: true
   has_many :users, through: :members
   has_many :members
 end
