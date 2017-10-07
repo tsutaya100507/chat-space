e416f4027efa694bb4bbb65d2376365a29bf9758
Tatsuya Ichikawa <tatsuyaichikawa@TatsuyanoMacBook-Pro.local>
2017-10-07 21:44:09 +0900

    edit_grougs_controller
    グループ作成のビューから入力値を取得すルため、編集した。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,15 +4,27 @@ class GroupsController < ApplicationController
   end
 
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, flash: {notice: 'グループを作成しました。'}
+    else
+      # binding.pry
+      render "new"
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  def group_params
+    params.require(:group).permit(:name,group_ids:[])
   end
 
 end
