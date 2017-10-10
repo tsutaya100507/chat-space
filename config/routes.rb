55fc72b5ad98dd01bb4cc993468d292bcd39ec01
Tatsuya Ichikawa <tatsuyaichikawa@TatsuyanoMacBook-Pro.local>
2017-10-10 14:07:07 +0900

    Delete comment_out
    不要なコメントアウトを削除した。

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
