71f592ab7119b8e155b3241004e9469f342c430b
Tatsuya Ichikawa <tatsuyaichikawa@TatsuyanoMacBook-Pro.local>
2017-10-11 17:59:52 +0900

    remove new_action
    メッセージ入力はindexページから直接行うこととしため、routes.rbのresourcesからnewを除外。

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update] do
-    resources :messages, only: [:index, :new, :create]
+    resources :messages, only: [:index, :create]
   end
 end
