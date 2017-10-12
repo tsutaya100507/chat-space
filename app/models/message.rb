08f3dba528aa1c339e24990f5658961534877ef5
Tatsuya Ichikawa <tatsuyaichikawa@TatsuyanoMacBook-Pro.local>
2017-10-13 08:31:04 +0900

    Add validation
    投稿時、メッセージと画像のどちらもなかった場合エラーが出るように設定した。

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,10 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
   mount_uploader :image, ImageUploader
+  validates :message_or_image, presence: true
+
+  private
+   def message_or_image
+    message.presence or image.presence
+  end
 end
